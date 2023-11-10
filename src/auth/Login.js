2e722be67a64802ecd85072c57416def85d2141d
Kari Rehe <karinicole84@gmail.com>
2023-11-09 19:10:54 -0500

    added password requirement to login page

--- src/auth/Login.js
+++ src/auth/Login.js
@@ -3,12 +3,16 @@ import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom"
 import "./Login.css"
 
+//check the img path on line 35 with Barry on Monday
+
 export const Login = () => {
-    const [email, set] = useState("me@me.com")
+    const [email, setEmail] = useState("me@me.com");
+    const [password, setPassword] = useState("");
     const navigate = useNavigate()
+    
     const handleLogin = (e) => {
         e.preventDefault()
-      return fetch(`http://localhost:8088/users?email=${email}`)
+      return fetch(`http://localhost:8088/users?email=${email}&password=${password}`)
        .then(res => res.json())
             .then(foundUsers => {
                 if (foundUsers.length === 1) {
@@ -25,20 +29,34 @@ export const Login = () => {
                 }
             })
     }
+    
+    
     return (
         <main className="container--login">
             <section>
                 <form className="form--login" onSubmit={handleLogin}>
                     <h1>Welcome</h1>
-                    <h2>Please sign in</h2>
+                    <img src="src/auth/images/Help!.png" alt="Help!" /> 
                     <fieldset>
-                        <label htmlFor="inputEmail"> Email address </label>
-                        <input type="email"
+                        <label htmlFor="inputEmail">Email address</label>
+                        <input
+                            type="email"
                             value={email}
-                            onChange={evt => set(evt.target.value)}
+                            onChange={evt => setEmail(evt.target.value)} 
                             className="form-control"
                             placeholder="Email address"
-                            required autoFocus />
+                            required autoFocus/>
+                    </fieldset>
+                    <fieldset>
+                        <label htmlFor="inputPassword">Password</label>
+                        <input
+                            type="password"
+                            value={password}
+                            onChange={evt => setPassword(evt.target.value)} 
+                            className="form-control"
+                            placeholder="Password" 
+                            required
+                        />
                     </fieldset>
                     <fieldset>
                         <button type="submit">
@@ -48,8 +66,7 @@ export const Login = () => {
                 </form>
             </section>
             <section className="link--register">
-                <Link to="/register">Not a member yet?</Link>
+                <Link to="/register">Create an account</Link>
             </section>
         </main>
-    )
-}
+    )}
